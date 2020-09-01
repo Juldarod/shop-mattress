507302de6651ee14271c097d4caffdac0f07349c
Julián D. Rodríguez <juliand.rodriguez@pragma.com.co>
2020-09-01 16:37:33 -0500

    Updates

---
+++ precache-manifest.9cb687730909fa4709736c1eb4d25527.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2150828a83d65d096def9aece9226799",
+    "url": "/shop-mattress/index.html"
+  },
+  {
+    "revision": "0357d568d42fdb9d0ccf",
+    "url": "/shop-mattress/static/css/main.58873086.chunk.css"
+  },
+  {
+    "revision": "f0f8584498c435b1703e",
+    "url": "/shop-mattress/static/js/2.c0b955b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shop-mattress/static/js/2.c0b955b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0357d568d42fdb9d0ccf",
+    "url": "/shop-mattress/static/js/main.5f7c399b.chunk.js"
+  },
+  {
+    "revision": "b750db1834089afd628e",
+    "url": "/shop-mattress/static/js/runtime-main.63ce872f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/shop-mattress/static/media/logo.ee7cd8ed.svg"
+  }
+]);
